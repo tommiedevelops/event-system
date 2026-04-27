51f4e7a9decc808af8720205de1073b3af3aeb58
tommiedevelops <thomas.dizon25@gmail.com>
2026-04-27 12:26:25 +1000

    added: some notes and a diagram

--- event.c
+++ event.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+void callback()
+{
+	printf("callback\n");
+}
+
+
+
+// Event handlers execute non-sequentially
+// Special care is needed to ensure event handlers are executed in the correct
+// order
+
 int main()
 {
 	printf("Hello, world!\n");
 
-	char[256] buf = {0};
+	// Event Loop (Listen for Events)
+	// Not the same as "Message Driven Programming"
 	while(true)
 	{
-		if(scanf("%s", buf));
+		// Callback Functions
+		callback();
 	}
 }
 
